d6c433f978f108c22df3c0cea3dccc48f2be4687
BijoyetaK <bijoyeta.euphoric@gmail.com>
2023-08-21 09:32:45 -0700

    Update 5_🗺️_Find_Dogs.py
    With plotly charts and select drop down for cities

--- pages/5_🗺️_Find_Dogs.py
+++ pages/5_🗺️_Find_Dogs.py
@@ -1,48 +1,73 @@
 import streamlit as st
 import pandas as pd
 #import numpy as np
-import folium
-from streamlit_folium import st_folium
+import plotly_express as px
+# import folium
+# from streamlit_folium import st_folium
+
+#st.set_page_config(layout="wide")
+
+def parse_int(text):
+    nbr = None
+    try:
+        nbr = int(text)
+    except:
+        nbr = None
+    return nbr
+
+def submitInput():
+    zipInput = st.session_state.get('zipInput')
+    if zipInput in zip_df['ZIP'].values:
+        mask = zip_df['ZIP'] == zipInput
+        userlat = zip_df.loc[mask,'LAT'].iloc[0]
+        userlon = zip_df.loc[mask,'LNG'].iloc[0]
+    else: # set to default location (berkeley,ca,usa)
+        userlat = 37.871593
+        userlon = -122.272743
+        selected = 94704
+    fig = map_func(zipInput, userlat, userlon)
+    st.plotly_chart(fig)
+
+def getZipforCity(location):
+    zip_list = []
+    shelters_df_location = shelters_df[shelters_df['location']==location]
+
+    if len(shelters_df_location) > 0:
+        zip_list = list(shelters_df_location['zip'].astype(int).drop_duplicates())
+
+        zip_list.sort()
+
+    return zip_list
 
 def map_func(startzip, startlat, startlon):
     """ Generate a folium map, display with st_folium"""
     # create map
-    m = folium.Map(location=[startlat, startlon], zoom_start=10)
+    #m = folium.Map(location=[startlat, startlon], zoom_start=10)
 
-    # create loop to write markers for every shelter in ~100 mi box
-    for lat,lon,name,phone,email,city,zipcode,country in zip(shelters_df['latitude'], shelters_df['longitude'], shelters_df['name'], 
-                                                             shelters_df['phone'], shelters_df['email'], shelters_df['city'], shelters_df['zip'],shelters_df['country']):
-        # check to see if zipcode is +- 1000
-        if (country == 'US'):
-            zipc = int(zipcode)
-            if (zipc > (startzip-1000)) and (zipc < (startzip + 1000)):
-                # get info for popup name, address, phone, email
-                pop_box = name
-                zipc = str(zipcode)
-                # input city, zipcode
-                if(type(city)==str):
-                    address ='\n' + city + ', ' + zipc
-                else:
-                    address ='\n' + zipc
-                
-                pop_box = pop_box + address
-                # input email
-                if (type(email)==str):
-                    pop_box = pop_box + '\n' + email
-                # input phone number
-                if(type(phone)==str):
-                    pop_box = pop_box + '\n' + phone
-                # pop_box updated
-                # add marker
-                folium.Marker(location=[lat,lon], tooltip = name, popup = pop_box).add_to(m)
-                # end for loop`
-    # call to render Folium map in Streamlit
-    return st_folium(m, width=725)
+    shelters_df['zip'] = shelters_df['zip'].apply(parse_int)
+    shelters_df_filtered = shelters_df[pd.notnull(shelters_df['zip'])]
+    shelters_df_filtered = shelters_df_filtered[(shelters_df_filtered['zip'] > (startzip-1000)) & (shelters_df_filtered['zip'] < (startzip + 1000)) & (shelters_df_filtered['country'] == 'US')]
+    shelters_df_filtered['size'] = 5
+
+    site_lat = shelters_df_filtered['latitude']
+    site_lon = shelters_df_filtered['longitude']
+    locations_name = shelters_df_filtered['name']
+
+
+    fig = px.scatter_mapbox(shelters_df_filtered, lat="latitude", lon="longitude", hover_name="name",hover_data=['city'],mapbox_style="open-street-map",width=725,height=800,
+                            color_discrete_sequence=["blue"],size ='size',zoom=6,opacity=0.5,title='Adoption Shelters around you')
+
+    fig.update_layout(margin={"r": 0, "l": 0, "b": 0},showlegend=False)
+    return fig
+
+
 
 # read in animal shelter csv into df
 file_path_s = "Data/input/petfinder_shelters.csv"
 shelters_df = pd.read_csv(file_path_s, sep=',')
 # address1,address2,city,country,email,id,latitude,longitude,name,phone,state,zip
+shelters_df['location'] = shelters_df['city'] + ' (' + shelters_df['state'] + ')'
+cities = list(shelters_df[shelters_df['country']=='US']['location'].drop_duplicates().sort_values())
 
 #read in zipcodes & lat/lon csv into df
 file_path_z = "Data/input/zip_lat_long.csv"
@@ -59,22 +84,34 @@ st.set_page_config(
 st.header(':green[Find a Pup Near You! :dog2:]')
 st.sidebar.header(':green[Find a Pup Near You! :dog2:]')
 
-with st.form(key='map_key'):
-    selected = st.number_input("Search by zipcode...", min_value=601, max_value = 99929, value=94704, help=':sparkles: Accepts US zipcodes from 00601 to 99929 :sparkles:')
-    st.write('The current zipcode is: ', selected)
-    sub = st.form_submit_button('Generate Map')
 
-# if zipcode was input to search
-if selected:
-    if selected in zip_df['ZIP'].values:
-        mask = zip_df['ZIP'] == selected
-        userlat = zip_df.loc[mask,'LAT'].iloc[0]
-        userlon = zip_df.loc[mask,'LNG'].iloc[0]
-    else: # set to default location (berkeley,ca,usa)
-        userlat = 37.871593
-        userlon = -122.272743
-        selected = 94704
-    
-    st_data = map_func(selected, userlat, userlon)
-else:
-    st.write('Page Error: 404 Perra')
+# select city - dropdown
+select_city = st.selectbox(label='Select City',options=cities)
+
+if select_city:
+    city_zips = getZipforCity(location=select_city)
+    if len(city_zips) > 0:
+
+        selected = st.number_input("Search by zipcode...",
+                                       min_value=city_zips[0], max_value = city_zips[-1],
+                                       value=city_zips[0],
+                                       help=':sparkles: Accepts US zipcodes from {0} to {1} :sparkles:'.format(str(city_zips[0]),str(city_zips[-1])),
+                                       key='zipInput'
+                                       )
+        if selected:
+            if selected in zip_df['ZIP'].values:
+                mask = zip_df['ZIP'] == selected
+                userlat = zip_df.loc[mask, 'LAT'].iloc[0]
+                userlon = zip_df.loc[mask, 'LNG'].iloc[0]
+            else:  # set to default location (berkeley,ca,usa)
+                userlat = 37.871593
+                userlon = -122.272743
+                selected = 94704
+
+            fig = map_func(selected, userlat, userlon)
+            st.plotly_chart(fig)
+
+
+    else:
+        st.warning("No shelters found")
+
